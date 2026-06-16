fc5f6da47e58d83134ae9e45e3d3ec546fceeb3a
Vicente Sanchez <vi.sanchezm@duocuc.cl>
2026-06-16 18:23:14 -0400

    style: limpiar repositorio para entrega final

--- 3-ms-gestion-java/src/main/java/com/duoc/msgestion/repository/ProductoRepository.java
+++
@@ -1,1 +0,0 @@
-package com.duoc.msgestion.repository; import com.duoc.msgestion.model.Producto; import org.springframework.data.jpa.repository.JpaRepository; import org.springframework.stereotype.Repository; @Repository public interface ProductoRepository extends JpaRepository<Producto, Long> { }
